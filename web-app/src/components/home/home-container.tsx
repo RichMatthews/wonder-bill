11b957eeba61cfa489527fcf8c4a253187314c5c
Rich Matthews <richmatthews@btinternet.com>
2020-11-09 20:00:53 +0000

    Fixes

--- web-app/src/components/home/home-container.tsx
+++ web-app/src/components/home/home-container.tsx
@@ -5,27 +5,29 @@ import { Link } from 'react-router-dom'
 import './index.css'
 
 export const Home = () => {
-    const payments = useSelector((store: any) => store.payments)
+    const payments = useSelector((store: { payments: any }) => store.payments)
 
     return (
         <div className="home-container">
             <div className="home-container-heading">Regular payments</div>
             <div className="payments-container">
-                {payments.map((payment: any) => {
-                    return (
-                        <Link to={`/edit/${payment.id}`} className="payment-container">
-                            <div className="payments-container-row payments-container-top-row">
-                                <div>{payment.name}</div>
-                                <div>£{payment.amount}</div>
-                            </div>
+                {payments.map(
+                    (payment: { id: string; name: string; amount: string; frequency: string; startDate: string }) => {
+                        return (
+                            <Link to={`/edit/${payment.id}`} className="payment-container">
+                                <div className="payments-container-row payments-container-top-row">
+                                    <div>{payment.name}</div>
+                                    <div>£{payment.amount}</div>
+                                </div>
 
-                            <div className="payments-container-row payments-container-bottom-row">
-                                <div>{payment.frequency}</div>
-                                <div>Next: {payment.startDate}</div>
-                            </div>
-                        </Link>
-                    )
-                })}
+                                <div className="payments-container-row payments-container-bottom-row">
+                                    <div>{payment.frequency}</div>
+                                    <div>Next: {payment.startDate}</div>
+                                </div>
+                            </Link>
+                        )
+                    },
+                )}
             </div>
             <Link to={'/add'} className="btn add-btn">
                 Add a bill
